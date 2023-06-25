dc286dd3ceb64adef098c497702623881e53564b
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-06-26 00:00:16 +0300

    UIView extension

--- ChatApp/Resources/Extensions.swift
+++ ChatApp/Resources/Extensions.swift
@@ -5,4 +5,31 @@
 //  Created by Admin on 24.06.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.origin.x + self.frame.width
+    }
+}
